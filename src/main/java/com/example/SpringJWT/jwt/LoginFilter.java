e70cf52c3abb00ba4290a615e862a4648280db9f
chobeebee <websera12@gmail.com>
2024-10-30 14:45:02 +0900

    로그인 성공 JWT 발급

--- src/main/java/com/example/SpringJWT/jwt/LoginFilter.java
+++ src/main/java/com/example/SpringJWT/jwt/LoginFilter.java
@@ -1,5 +1,6 @@
 package com.example.SpringJWT.jwt;
 
+import com.example.SpringJWT.dto.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,14 +8,23 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
 
-    public LoginFilter(AuthenticationManager authenticationManager) {
+    //JWTUtil 주입
+    private final JWTUtil jwtUtil;
+
+    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+
         this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
@@ -34,12 +44,31 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-        System.out.println("success");
+        //UserDetailsS
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal(); //유저 확인
+
+        String username = customUserDetails.getUsername(); //username 추출
+
+        //role 값 추출
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+        
+        //JWT 토큰 생성
+        String token = jwtUtil.createJwt(username, role, 60*60*10L);
+
+        //JWT를 Header에 담아서 응답
+        //무조건, 인증방식을 접두사로 붙여서 응답해줘야 함(공식적으로 정의 됨)
+        response.addHeader("Authorization", "Bearer " + token);
     }
 
     //로그인 실패시 실행하는 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
-        System.out.println("fail");
+
+        //로그인 실패시 401 응답 코드 반환
+        response.setStatus(401);
     }
 }
